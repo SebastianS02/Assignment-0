a0480a3b1b212b6e9415db865920e36c2203e1ab
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-26 22:47:22 -0500

    final commit

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,11 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  let count = 0; //initialize count
+  for(let i = 0; i < nums.length; i++){ //run through array
+    if(nums[i] >= start && nums[i] <= end){ //checks if element is in range
+      count++; //increment count if above statement is true
+    }
+  }
+  return count; //returns number of elements in range
 }
 
 // Do not edit this line;
