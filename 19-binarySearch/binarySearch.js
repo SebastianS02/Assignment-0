c91a54103651686e7470e23126f0b9792816a214
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-27 12:55:11 -0500

    final commit

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,7 +4,18 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+    let mid = (Math.floor(nums.length/2)); //Set mid value for binary search
+    let arr = nums.slice(0); //Copies array so original is not mutated
+    if(nums[mid] == target){ //If key is found, return true
+      return true;
+    }
+    if(nums[mid] < target){ //If mid is less than target, check upper half of array
+      return this.binarySearch(arr.splice(mid, arr.length - 1), target);
+    }
+    if(nums[mid] > target){ //If mid is greater than target, check lower half of array
+      return this.binarySearch(arr.splice(0, mid), target);
+    }
+    return false; //target was never found
   }
 }
 
