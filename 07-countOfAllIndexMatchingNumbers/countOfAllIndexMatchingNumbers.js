66badd732ca8ad38c45005bee99fa866fe33f5ba
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-26 22:20:24 -0500

    final commit

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,11 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  let count = 0;  //Initialize count
+  for(let i = 0; i < nums.length; i++){  //Go through each element in the array
+    if(nums[i] == i){  //Checks if current element is equal to its index
+      count++;  //Increments count if above statement is true
+    }
+  }
+  return count; //returns number of elements that match the index
 }
 
 // Do not edit this line;
