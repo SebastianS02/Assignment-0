d2ee4dcfb833b77c0a7e7e6345ab0a9c109d9e14
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-26 22:28:11 -0500

    final commit

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,11 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  let count = 0; //Initialize count
+  for(let i = 0; i < nums.length; i++){  //runs through array
+    if(nums[i] % 2 == 0){  //checks if element is even
+      count++; //Increment count if above statement is true
+    }
+  }
+  return count; //Returns number of even numbers in array
 }
 
 // Do not edit this line;
