6497974714638a9925b0e92fdbe469eabf2a73f4
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-27 11:12:18 -0500

    final commit

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,15 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  var frq = {}; //Initialize the object
+  for(let i = 0; i < word.length; i++){ //run through string/word
+    let character = word.charAt(i); //Obtains character at current element position
+    if(frq[character]){ //If the character exists and another is found
+      frq[character]++; //Increment character counter
+    }
+    else{ 
+      frq[character] = 1; //New character was found
+    }
+  }
+  return frq; //Returns frequencies
 }
 
 // Do not edit this line;
