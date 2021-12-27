95647752cf7ef082c2bfc35351810c7748ec25c6
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-27 10:36:39 -0500

    final commit

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
-  // Insert code here;
+  let len = word.length; //Initialize length of the string
+  for(let i = 0; i < len/2; i++){ //run through half of the word
+    if(word[i] != word[len - i - 1]){ //Checks if the halves of the word are NOT equal
+      return false; //Not a palindrome, ends code
+    }
+  }
+  return true; //Is a palindrome
 }
 
 // Do not edit this line;
