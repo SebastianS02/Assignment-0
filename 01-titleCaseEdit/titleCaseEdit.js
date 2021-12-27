e86cdb345560a58daff0ac95c8930eda04489e0f
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-26 21:26:14 -0500

    final commit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,10 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  const arr = title.split(" ");     //Splits title at every space
+  for(let i = 0; i < arr.length; i++){  //runs through each word in the title
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1); //Capitalizes first index of each array (each word)
+  }
+  const revisedTitle = arr.join(" "); //Combines the title after being split into separate arrays
+  return revisedTitle; //return new and improved title
 }
 
 // Do not edit this line;
