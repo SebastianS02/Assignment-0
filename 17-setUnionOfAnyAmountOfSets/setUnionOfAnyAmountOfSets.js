6b5b019d61037f7232675d7ab9f8bbd1e189983f
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-27 12:09:54 -0500

    final commit

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,11 @@
-function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+function setUnionOfAnyAmountOfSets(...args){
+  let newSet = new Set; //Initialize new set
+  for(let i = 0; i < args.length; i++){ //Run through list of arguments
+    for(const element of args[i]){ //Run through each set in the arguments
+      newSet.add(element); //.add only adds to new set if element doesn't already exist in that set
+    }
+  }     
+  return newSet; //returns union of sets
 }
 
 // Do not edit this line;
