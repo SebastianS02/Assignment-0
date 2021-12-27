e1ff7fc0e6a111e1f5246777f34cbadc8a24380b
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-27 08:47:36 -0500

    final commit

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,17 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  let min = nums[0];
+  let max = nums[0];
+  let sum = 0; //Initialzing values
+  for(let i = 0; i < nums.length; i++){ //run through array
+    if(min > nums[i]){ //Checks if current element is less than minimum value
+      min = nums[i]; //Sets new minimum value if above statement is true
+    }
+    if(max < nums[i]){ //Checks if current element is greater than maximum value
+      max = nums[i]; //Sets new maximum value if above statement is true;
+    }
+  }
+  sum = min + max; //Adds the minimum and maximum values
+  return sum; //Returns above value
 }
 
 // Do not edit this line;
