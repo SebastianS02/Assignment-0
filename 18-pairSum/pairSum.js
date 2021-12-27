a950b8fd4f8ff91ef18596b88e33ae3c0ab83f38
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-27 12:20:15 -0500

    final commit

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,17 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  if(nums.length <= 1){ //Checks if array has less than or 1 element
+    throw 'error'; //Throws error if above statement is true
+  }
+  else{ //runs code otherwise
+  for(let i = 0; i < nums.length; i++){  //Runs through array for first number
+    for(let j = 0; j < nums.length; j++){ //Runs through array for second number
+      if(nums[i] + nums[j] == target){ //Checks if two values add up to target
+        return true; //Returns true if above statement is true, ends code
+      }
+    }
+  }
+  }
+  return false; //All variations were checked and no sum was found, returns false
 }
 
 // Do not edit this line;
