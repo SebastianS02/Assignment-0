f8996e52b018f5fbb34e8c65b973720569c40fef
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-26 22:10:17 -0500

    final commit

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,11 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  let count = 0; //initialize count
+  for(let i = 0; i < arr.length; i++){ //go through array of variables
+    if(typeof arr[i] == "boolean" || typeof arr[i] == "string"){ //checks if the current element is of type boolean or string
+      count++; //Increment count if variable is a boolean or a string
+    }
+  }
+  return count; //return number of booleans and strings in arr
 }
 
 // Do not edit this line;
