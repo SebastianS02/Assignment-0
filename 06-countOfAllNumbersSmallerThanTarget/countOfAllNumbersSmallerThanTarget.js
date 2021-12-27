8aa4f1aace84948d91b4e37e0244c15b68921fc5
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-26 22:16:11 -0500

    final commit

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,11 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  let count = 0; //Initialize count
+  for(let i = 0; i < nums.length; i++){  //Go through array of nums
+    if(nums[i] < target){  //Check if current element is smaller than target
+      count++; //Increment count if above statement is true
+    }
+  }
+  return count;  //returns number of values smaller than target
 }
 
 // Do not edit this line;
