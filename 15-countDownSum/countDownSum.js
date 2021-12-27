ec935bc87622a815efe39b4706d12d05f73fd5db
SebastianS02 <stecsebastian2002@gmail.com>
2021-12-27 11:23:30 -0500

    final commit

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,11 @@
 class MySolution {
   countDownSum(num) {
-    // Insert code here;
+    if(num <= 1){ //Base case, checks if num is less than or equal to 1
+      return num; //Returns num if above statement is true
+    }
+    else{ //Num is not less than or equal to 1
+      return num + this.countDownSum(num-1); //Recursively return current num plus one before it, hence "-1"
+    }
   }
 }
 
